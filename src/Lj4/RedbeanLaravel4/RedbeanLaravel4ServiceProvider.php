fab4d99b6c100776b8a17453e8f3fb9cddab2ac0
JJ Johnson <valedaemon@gmail.com>
2013-06-04 01:53:11 -0400

    Changed setup call

--- src/Lj4/RedbeanLaravel4/RedbeanLaravel4ServiceProvider.php
+++ src/Lj4/RedbeanLaravel4/RedbeanLaravel4ServiceProvider.php
@@ -36,13 +36,15 @@ class RedbeanLaravel4ServiceProvider extends ServiceProvider {
 		$db_user = $connections[$default]['username']; 
 		$db_pass = $connections[$default]['password'];
 		$db_name = $connections[$default]['database'];
-		Log::error(var_dump($default));
+		$db_driver = $connections[$default]['driver'];
+
 		//Run the R::setup command based on db_type
 		if ($default != 'sqlite') {
-			R::setup("'".$default.":host=".$db_host.";dbname=".$db_name."','".$db_user."','".$db_pass."'");
+			$conn_string = $db_driver.':host='.$db_host.';dbname='.$db_name;
 		} else {
-			R::setup("'".$default.":".$db_name."','".$db_user."','".$db_pass."'");
+			$conn_string = $db_driver.':'.$db_name;
 		}
+		R::setup($conn_string, $db_user, $db_pass);
 	}
 
 	/**
